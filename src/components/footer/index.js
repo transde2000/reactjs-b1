83f782733ad24d7a53c1e9add9460541649c6967
De Tran <transude2000@gmail.com>
2023-03-10 20:33:37 +0700

    init

---
+++ src/components/footer/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import './style.css';
+
+const Footer = () => {
+    const stylebject = {
+        backgroundColor: "yellow",
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        width: "100%",
+    };
+  return (
+    <div style={stylebject}>
+      <h1>This Footer</h1>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/header/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+function index() {
+  return (
+    <div className='header'>
+      <h1>Title Example React</h1>
+      <button className='button'>click</button>
+    </div>
+  )
+}
+export function Header() {
+  return (
+    <div className='header'>
+      <h1>Title Example React Native</h1>
+      <button className='button'>click</button>
+    </div>
+  )
+}
+export default index;
